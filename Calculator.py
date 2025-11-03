086c71d05321292dabfc20edcc7d8c88c6598fbf
HafsaAbid82 <hafsaabd.82@gmail.com>
2025-11-03 04:48:50 -0800

    Changes added

--- Calculator.py
+++ Calculator.py
@@ -13,33 +13,48 @@ try:
 except:
     print("Invalid Input. Please enter an integer.")
 else:
-    if (option in [1,2,3,4,5,6,]):
-        num1 = int(input('Enter First Number: '))
-        num2 = int(input('Enter Second Number: '))
-        if (option == 1):
-            result = num1 + num2
-        elif(option == 2):
-            result = num1 - num2
-        elif(option == 3):
-            result = num1 * num2
-        elif(option == 4):
-           result = num1 // num2
-        elif(option == 5):
-           result = num1 % num2
-        elif(option == 6):
-           result = num1 ** num2
-    elif (option in [7,]):
-        if (option == 7):
-              num = float(input("Enter a number: "))
-              result = num ** 0.5
+    if (option in [1,2,3,4,5,6]):
+        try:
+            num1 = int(input('Enter First Number: '))
+            num2 = int(input('Enter Second Number: '))
+            if (option == 1):
+                result = num1 + num2
+            elif(option == 2):
+                result = num1 - num2
+            elif(option == 3):
+                result = num1 * num2
+            elif(option == 4):
+                if num2 == 0:
+                    print("Error: Division by zero is not allowed.")
+                else:
+                    result = num1 // num2
+            elif(option == 5):
+                result = num1 % num2
+            elif(option == 6):
+                result = num1 ** num2
+        except:
+            print("Invalid Input. Please enter valid numbers.")
+    elif (option in [7]):
+        try:
+            num = float(input("Enter a number: "))
+            if num < 0:
+                print("Error: Cannot take square root of a negative number.")
+            else:
+                result = num ** 0.5
+        except:
+            print("Invalid Input. Please enter a valid number.")   
     elif (option in [8,9]):
-        num = int(input('Enter a Number: '))
-        if (option == 8):
-           num += 1
-           result = num
-        elif(option == 9):
-           num -= 1
-           result = num 
+        try:
+            num = int(input('Enter a Number: '))
+            if (option == 8):
+                num += 1
+                result = num
+            elif(option == 9):
+                num -= 1
+                result = num 
+        except:
+            print("Invalid Input. Please enter an integer.")
     else:
-     print("Invalid Number")
+        print("Invalid Number")
 print("The Result of the operation is: {}".format(result))
+
